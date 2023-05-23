f4750ae1d9d0ed9633af0bc10f883c38ef8b1e84
Braun_Alex <lamanasakasa@gmail.com>
2023-05-24 00:42:50 +0300

    Fix has been made

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     if (!RUN_ALL_TESTS()) {
         std::cout << "All the FIPS-140-3 tests for randomly generated sequence of 20000 bits have been passed";
     } else {
-        std::cout << "Randomly generated sequence of 20000 bits is not random according to FIPS-140-3 standard";
+        std::cout << "Randomly generated sequence of 20000 bits is not random according to FIPS-140-3";
     }
     return 0;
 }
